e287d6728f2e2449b64dc535faa45aec3c1baef2
Hengrip <68610577+TheOGHengrip@users.noreply.github.com>
2023-06-02 15:51:06 -0600

    Add files via upload

---
+++ src/main/java/com/codingstuff/loginandsignup/MascotaModelo.kt
@@ -0,0 +1,11 @@
+package com.codingstuff.loginandsignup
+
+data class MascotaModelo (
+    var mascotaId: String? = null,
+    var mascotaNom: String? = null,
+    var mascotaSexo: String? = null,
+    var mascotaEdad: String? = null,
+    var mascotaRaza: String? = null,
+    var imgUri: String? = null
+
+    )
