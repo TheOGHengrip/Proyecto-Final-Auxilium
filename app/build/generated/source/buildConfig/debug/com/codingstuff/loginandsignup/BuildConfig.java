089d7ce3c333ce242b0b763829dc56d3c5ae6890
Hengrip <68610577+TheOGHengrip@users.noreply.github.com>
2023-06-02 15:59:44 -0600

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/com/codingstuff/loginandsignup/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.codingstuff.loginandsignup;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.codingstuff.loginandsignupp";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
